a5307a53b2dde4d4655d24be7800f37b08c2ee6e
Chaitanya Kharche <kchaitanya1331@gmail.com>
2024-07-24 07:41:22 -0400

    Enhanced UI/UX with Material-UI

--- frontend/src/components/CompanyDetails.js
+++ frontend/src/components/CompanyDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import { Container, Typography, Card, CardContent, List, ListItem, ListItemText, Button } from "@mui/material";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -22,30 +23,49 @@ function CompanyDetails() {
     if (!company) return <div>Loading...</div>;
 
     return (
-        <div>
-            <h1>{company.name}</h1>
-            <p>{company.address}</p>
-            <MapContainer center={[company.latitude, company.longitude]} zoom={13} style={{ height: "400px", width: "100%" }}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+        <Container>
+            <Typography variant="h4" gutterBottom>
+                {company.name}
+            </Typography>
+            <Card>
+                <CardContent>
+                    <Typography variant="h6">
+                        Address
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {company.address}
+                    </Typography>
+                    <MapContainer center={[company.latitude, company.longitude]} zoom={13} style={{ height: "400px", width: "100%" }}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        {locations.map(location => (
+                            <Marker key={location.location_id} position={[location.latitude, location.longitude]}>
+                                <Popup>
+                                    {location.name}<br />{location.address}
+                                </Popup>
+                            </Marker>
+                        ))}
+                    </MapContainer>
+                </CardContent>
+            </Card>
+            <Typography variant="h6" gutterBottom>
+                Locations
+            </Typography>
+            <List>
                 {locations.map(location => (
-                    <Marker key={location.location_id} position={[location.latitude, location.longitude]}>
-                        <Popup>
-                            {location.name}<br />{location.address}
-                        </Popup>
-                    </Marker>
+                    <ListItem key={location.location_id}>
+                        <ListItemText
+                            primary={location.name}
+                            secondary={location.address}
+                        />
+                    </ListItem>
                 ))}
-            </MapContainer>
-            <ul>
-                {locations.map(location => (
-                    <li key={location.location_id}>
-                        {location.name} - {location.address}
-                    </li>
-                ))}
-            </ul>
-            <Link to="/">Back to List</Link>
-        </div>
+            </List>
+            <Button variant="contained" color="primary" component={Link} to="/" style={{ marginTop: "16px" }}>
+                Back to List
+            </Button>
+        </Container>
     );
 }
 
--- frontend/src/components/CompanyList.js
+++ frontend/src/components/CompanyList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Container, Typography, List, ListItem, ListItemText, TextField } from "@mui/material";
 
 function CompanyList() {
     const [companies, setCompanies] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         axios.get("http://localhost:8000/companies")
@@ -11,19 +13,33 @@ function CompanyList() {
             .catch(error => console.error("Error fetching companies:", error));
     }, []);
 
+    const filteredCompanies = companies.filter(company =>
+        company.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
-        <div>
-            <h1>Company List</h1>
-            <ul>
-                {companies.map(company => (
-                    <li key={company.company_id}>
-                        <Link to={`/company/${company.company_id}`}>
-                            {company.name} - {company.address}
-                        </Link>
-                    </li>
+        <Container>
+            <Typography variant="h4" gutterBottom>
+                Company List
+            </Typography>
+            <TextField
+                label="Search Companies"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <List>
+                {filteredCompanies.map(company => (
+                    <ListItem button component={Link} to={`/company/${company.company_id}`} key={company.company_id}>
+                        <ListItemText
+                            primary={company.name}
+                            secondary={company.address}
+                        />
+                    </ListItem>
                 ))}
-            </ul>
-        </div>
+            </List>
+        </Container>
     );
 }
 
